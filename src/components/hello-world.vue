cf1cfb280725de78f2130d9d13d28a565cfb6480
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-02-18 18:41:30 +0200

    Update hello-world.vue

--- src/components/hello-world.vue
+++ src/components/hello-world.vue
@@ -35,8 +35,13 @@
                         </v-alert>
                     </template>
                 </v-data-table>
-
-              <v-fab-transition>
+              <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="600px"
+    >
+      <template v-slot:activator="{ on, attrs }">
+       <v-fab-transition>
               <v-btn
                 color="primary"
                 dark
@@ -44,10 +49,76 @@
                 top
                 right
                 fab
+                v-bind="attrs"
+                v-on="on" 
               >
+               <span class="mr-2"></span>
                 <v-icon>mdi-plus</v-icon>
               </v-btn>
             </v-fab-transition>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">comment</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                  label="number"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col
+              
+              <v-col cols="12">
+                <v-text-field
+                  v-model="comment.email"
+                  label="Email*"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  v-model="comment.message"
+                  label="Message*"
+                  type="textarea"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+              >
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="addcomment"
+          >
+            Close
+          </v-btn>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialog = false"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+              
             </v-flex>
         </v-layout>
     </v-container>
