8f80541ab6a67a3ea9999c76d4227ae27964961a
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-02-18 17:19:02 +0200

    Update app.vue

--- src/app.vue
+++ src/app.vue
@@ -8,7 +8,7 @@
             <v-spacer />
             <v-btn
                 flat
-                href=""
+                href="#add"
                 target="_blank">
                 <span class="mr-2"></span>
                 <v-icon>mdi-plus</v-icon>
@@ -18,6 +18,92 @@
         <v-content>
             <router-view />
         </v-content>
+      
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="600px"
+      id="add"
+    >
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">User Profile</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                  label="Legal first name*"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                  label="Legal middle name"
+                  hint="example of helper text only on focus"
+                ></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                  label="Legal last name*"
+                  hint="example of persistent helper text"
+                  persistent-hint
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  label="Email*"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  label="Password*"
+                  type="password"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+              >
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialog = false"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     </v-app>
 </template>
 
@@ -27,7 +113,7 @@ export default {
     name: 'App',
     data () {
         return {
-            //
+            dialog:false
         };
     },
 };
