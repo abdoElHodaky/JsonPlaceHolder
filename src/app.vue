ed198fd33bb7cef69e3b510bf50b68a4ec30593f
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-02-18 16:40:20 +0200

    Update app.vue

--- src/app.vue
+++ src/app.vue
@@ -3,7 +3,7 @@
         <v-toolbar app>
             <v-toolbar-title class="headline text-uppercase">
                 <span>Task</span>
-                <span class="font-weight-light">MATERIAL DESIGN</span>
+                <span class="font-weight-light"></span>
             </v-toolbar-title>
             <v-spacer />
             <!--v-btn
