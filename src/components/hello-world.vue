8da03930ea8af06c2f6785e2d36f88f87f3cf8c0
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-02-19 14:44:27 +0200

    Update hello-world.vue

--- src/components/hello-world.vue
+++ src/components/hello-world.vue
@@ -23,6 +23,7 @@
                         <td class="text-xs-left">
                             {{ props.item.body }}
                         </td>
+                      <template v-slot:item.actions="{ props.item }">
                       <td class="text-xs-left">
                       <v-icon
                      small
@@ -40,7 +41,7 @@
                      mdi-delete
                      </v-icon>
                      </td>
-                     
+                     </template>
                       
                     </template>
                     
